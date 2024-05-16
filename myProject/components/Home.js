e176f7204e2d8affe3082d2dd278e968c18987e8
Vegito2367 <tejaaditya.g@gmail.com>
2024-05-16 14:19:51 -0500

    Added basic item tracking

--- myProject/components/Home.js
+++ myProject/components/Home.js
@@ -38,60 +38,70 @@ const styles = StyleSheet.create({
 
 const testImages=[
   {
+    id: 1,
     name: "Apple",
     image:require("../public/tejPic.jpg"),
     availableCount: 5,
     reservedCount: 2
   },
   {
+    id: 2,
     name: "Apple",
     image:require("../public/tejPic.jpg"),
     availableCount: 5,
     reservedCount: 2
   },
   {
+    id: 3,
     name: "Milk",
     image:require("../public/tejPic.jpg"),
     availableCount: 3,
     reservedCount: 1
   },
   {
+    id: 4,
     name: "Beans",
     image:require("../public/tejPic.jpg"),
     availableCount: 9,
     reservedCount: 4
   },
   {
+    id: 5,
     name: "Chips",
     image:require("../public/tejPic.jpg"),
     availableCount: 10,
     reservedCount: 0
   },
   {
+    id: 6,
     name: "Apple",
     image:require("../public/tejPic.jpg"),
     availableCount: 5,
     reservedCount: 2
   },
   {
+    id: 7,
     name: "Apple",
     image:require("../public/tejPic.jpg"),
     availableCount: 5,
     reservedCount: 2
   },
   {
+    id: 8,
     name: "Milk",
     image:require("../public/tejPic.jpg"),
     availableCount: 3,
     reservedCount: 1
   },
   {
+    id: 9,
     name: "Beans",
     image:require("../public/tejPic.jpg"),
     availableCount: 9,
     reservedCount: 4
   },
   {
+    id: 10,
     name: "Chips",
     image:require("../public/tejPic.jpg"),
     availableCount: 10,
@@ -107,9 +117,11 @@ export function HomeScreen({ navigation })
   const [text, setText] = React.useState('');
 
   const [modalVisibility, setModalVisibility] = useState(false);
-  const [data, setData] = useState(null);
+  const [modalData, setModalData] = useState(null);
+  const [userCount, setUserCount] = useState(0);
   function handlePress(param){
-    setData(param);
+    console.log(param)
+    setModalData(param);
     setModalVisibility(true);
   }
 
@@ -120,6 +132,30 @@ export function HomeScreen({ navigation })
   {
     navigation.navigate('Profile', {name: text});
   }
+
+  function AddNumber(){
+    setUserCount(userCount+1);
+  }
+  function SubtractNumber(){
+    if(userCount>0){
+      setUserCount(userCount-1);
+    }
+  }
+
+  function handleModalClose(){
+    
+    if(userCount>modalData.availableCount){
+      alert("You cannot reserve more than available");
+      return;
+    }
+    else if(userCount>0){
+      alert("You have reserved " + userCount + " " + modalData.name);
+      testImages[modalData.id-1].availableCount-=userCount;
+      setModalData(null);
+      setUserCount(0);
+    }
+    closeModal();
+  }
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Modal visible={modalVisibility} animationType="slide" transparent={true}>
@@ -128,27 +164,27 @@ export function HomeScreen({ navigation })
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
               <Text>How many do you want to reserve?</Text>
-              <Image style={{width:100, height:100}} source={data?.image}/>
-              <Text>{data?.name}</Text>
-              <Text>{data?.availableCount}</Text>
-              <Text>{data?.reservedCount}</Text>
+              <Image style={{width:100, height:100}} source={modalData?.image}/>
+              <Text>{modalData?.name}</Text>
+              <Text>{modalData?.availableCount}</Text>
+              <Text>{modalData?.reservedCount}</Text>
               <View style={{display:"flex", flexDirection:"row", alignContent:"center", }}>
-              <Button title="+" style={{backgroundColor:"rgba(99, 111, 130,1)"}}></Button>
-              <View style={{backgroundColor:"rgba(99, 111, 130,1)", padding:10, marginLeft:20, marginRight:20}}><Text adjustsFontSizeToFit={true} style={{fontSize:23}}>1</Text></View>
-              <Button title="-" style={{backgroundColor:"rgba(99, 111, 130,1)"}}></Button>
+              <Button title="+" onPress={AddNumber} style={{backgroundColor:"rgba(99, 111, 130,1)"}}></Button>
+              <View style={{backgroundColor:"rgba(99, 111, 130,1)", padding:7.5, marginLeft:20, marginRight:20}}><Text adjustsFontSizeToFit={true} style={{fontSize:23}}>{userCount}</Text></View>
+              <Button title="-" onPress={SubtractNumber} style={{backgroundColor:"rgba(99, 111, 130,1)"}}></Button>
               </View>
-              <Button title="Close" onPress={closeModal}/>
+              <Button title="Done" onPress={handleModalClose}/>
             </View>  
           </View>
         </View>
       </Modal>
       <View style={{display:"flex", flexDirection:"row", flexWrap:"wrap"}}>
       {testImages.map((item, index) => (
-        <ItemHolder name={item.name} image={item.image} reservedCount={item.reservedCount} availableCount={item.availableCount} onClickFunction={handlePress} key={index} />
+        <ItemHolder name={item.name} image={item.image} reservedCount={item.reservedCount} availableCount={item.availableCount} onClickFunction={handlePress} key={index} id={item.id} />
       ))}
       </View>
 
-      
+
     </ScrollView>
   );
 }
